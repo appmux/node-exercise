f21af0835347d1115b809365afa64f2ce865c1f7
appmux <appmux@gmail.com>
2016-06-08 18:00:38 -0400

    Code clean-up.

--- src/server/modules/core/index.js
+++ src/server/modules/core/index.js
@@ -4,13 +4,5 @@ import getRoutes from './core.route.js';
 export { getRoutes };
 
 export function factory(sm) {
-    this.dispatcher = sm.get('dispatcher');
-
-    this.dispatcher.subscribe('onRoute', onRoute);
-
     return this;
-}
-
-function onRoute(req, res, route) {
-    console.log('onRoute', route);
 }
